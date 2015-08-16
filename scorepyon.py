b6e09b18976b6eb8bd3b83eb6675610bc39d9dac
Ryan Foley <rtfoley@gmail.com>
2015-08-15 22:29:39 -0400

    Refactored score form to use wtform and flask-wtf.

--- scorepyon.py
+++ scorepyon.py
@@ -1,16 +1,26 @@
 import flask
 from flask import render_template, request, jsonify
+from flask.ext.wtf import Form
+from wtforms import SelectField, BooleanField
 
 app = flask.Flask(__name__)
+app.config.from_object('config')
+
+class ScoreForm(Form):
+    treebranchcloser = BooleanField('treebranchcloser', default=False)
+    treebranchintact = BooleanField('treebranchintact', default=False)
+    cargoplane = SelectField('cargoplane', choices=[('0', 'None'), ('1', 'Yellow only'), ('2', 'Light blue')])
 
 @app.route('/_add_numbers')
 def add_numbers():
-    treebranchcloser = request.args.get('treebranchcloser', 0, type=int)
-    treebranchintact = request.args.get('treebranchintact', 0, type=int)
+    treebranchcloser = request.args.get('treebranchcloser')
+    treebranchintact = request.args.get('treebranchintact')
     cargoplane = request.args.get('cargoplane', 0, type=int)
 
     score = 0
-    score += 30 if treebranchcloser == 1 and treebranchintact == 1 else 0
+    if treebranchcloser=='true' and treebranchintact=='true':
+        print 'tree success'
+        score += 30
     score += get_plane_score(cargoplane)
 
     return jsonify(result=score)
@@ -25,7 +35,8 @@ def get_plane_score(argument):
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    form = ScoreForm()
+    return render_template("index.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug = True)

46bb0ba5adab8bba9c17e783c2117a2dfba03435
Ryan Foley <rtfoley@gmail.com>
2015-10-06 12:43:11 -0400

    Add basic validation to team form using new form helpers HTML

--- forms.py
+++ forms.py
@@ -1,14 +1,17 @@
 from flask.ext.wtf import Form
-from wtforms import SelectField, BooleanField, IntegerField, TextField
+from wtforms import SelectField, BooleanField, IntegerField, TextField, \
+    validators
 
 
 # TODO add vailidation
 class TeamForm(Form):
-    number = IntegerField()
-    name = TextField()
-    affiliation = TextField()
-    city = TextField()
-    state = TextField()
+    number = IntegerField("Number", [validators.Required(),
+                                     validators.NumberRange(min=1, max=99999)])
+    name = TextField("Name", [validators.Required(),
+                              validators.Length(min=1, max=50)])
+    affiliation = TextField("Affiliation", [validators.Length(min=1, max=200)])
+    city = TextField("City", [validators.Length(min=1, max=50)])
+    state = TextField("State", [validators.Length(min=2, max=2)])
 
 
 class ScoreForm(Form):

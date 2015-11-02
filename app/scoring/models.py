c179d41bc50d14d898b80948a609b9fa61a2b359
Ryan Foley <rtfoley@gmail.com>
2015-11-01 22:08:49 -0500

    Restructure application using Flask Blueprints for better code organization

---
+++ app/scoring/models.py
@@ -0,0 +1,40 @@
+from app import db
+
+
+# Robot score behavior and calculation
+class RobotScore(db.Model):
+    __tablename__ = 'robot_scores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
+    round_number = db.Column(db.Integer)
+    tree_branch_is_closer = db.Column(db.Boolean)
+    tree_branch_is_intact = db.Column(db.Boolean)
+    cargo_plane_location = db.Column(db.Integer)
+
+    def __init__(self,
+                 team=0,
+                 round_number=1,
+                 tree_branch_is_closer=False,
+                 tree_branch_is_intact=False,
+                 cargo_plane_location=0):
+        self.team_id = team
+        self.round_number = round_number
+        self.tree_branch_is_closer = tree_branch_is_closer
+        self.tree_branch_is_intact = tree_branch_is_intact
+        self.cargo_plane_location = cargo_plane_location
+
+    def getScore(self):
+        score = 0
+        if self.tree_branch_is_closer and self.tree_branch_is_intact:
+            score += 30
+        score += self.get_plane_score(self.cargo_plane_location)
+        return score
+
+    def get_plane_score(self, argument):
+        switcher = {
+            0: 0,
+            1: 20,
+            2: 30,
+            }
+        return switcher.get(argument, 0)

5da7a168077e3a8b718cc874bde5dfa54ab1079c
Ryan Foley <rtfoley@gmail.com>
2015-11-06 13:18:31 -0500

    Update judging forms to use new macros and button group

--- app/judging/forms.py
+++ app/judging/forms.py
@@ -1,47 +1,46 @@
 from flask.ext.wtf import Form
-from wtforms import SelectField, SubmitField
+from wtforms import SelectField, RadioField
 from .models import Presentation, Technical, Teamwork, TeamSpirit
 
 
 class PresentationForm(Form):
     team_id = SelectField(u'Team', coerce=int)
-    problem_identification = SelectField(u'Problem identification',
-                                         choices=[(i, i) for i in range(0, 5)],
-                                         coerce=int)
-    sources_of_information = SelectField(u'Sources of information',
-                                         choices=[(i, i) for i in range(0, 5)],
-                                         coerce=int)
-    problem_analysis = SelectField(u'Problem analysis',
-                                   choices=[(i, i) for i in range(0, 5)],
-                                   coerce=int)
-    existing_solutions = SelectField(u'Existing solutions',
-                                     choices=[(i, i) for i in range(0, 5)],
-                                     coerce=int)
-    team_solution = SelectField(u'Team solution',
-                                choices=[(i, i) for i in range(0, 5)],
-                                coerce=int)
-    innovation = SelectField(u'Innovation',
-                             choices=[(i, i) for i in range(0, 5)],
-                             coerce=int)
-    implementation = SelectField(u'Implementation',
-                                 choices=[(i, i) for i in range(0, 5)],
-                                 coerce=int)
-    sharing = SelectField(u'Sharing',
-                          choices=[(i, i) for i in range(0, 5)],
-                          coerce=int)
-    creativity = SelectField(u'Creativity',
-                             choices=[(i, i) for i in range(0, 5)],
-                             coerce=int)
-    effectiveness = SelectField(u'Effectiveness',
-                                choices=[(i, i) for i in range(0, 5)],
-                                coerce=int)
-    inclusion = SelectField(u'Inclusion',
+    problem_identification = RadioField(u'Problem identification',
+                                        choices=[(i, i) for i in range(0, 5)],
+                                        coerce=int)
+    sources_of_information = RadioField(u'Sources of information',
+                                        choices=[(i, i) for i in range(0, 5)],
+                                        coerce=int)
+    problem_analysis = RadioField(u'Problem analysis',
+                                  choices=[(i, i) for i in range(0, 5)],
+                                  coerce=int)
+    existing_solutions = RadioField(u'Existing solutions',
+                                    choices=[(i, i) for i in range(0, 5)],
+                                    coerce=int)
+    team_solution = RadioField(u'Team solution',
+                               choices=[(i, i) for i in range(0, 5)],
+                               coerce=int)
+    innovation = RadioField(u'Innovation',
                             choices=[(i, i) for i in range(0, 5)],
                             coerce=int)
-    respect = SelectField(u'Respect',
-                          choices=[(i, i) for i in range(0, 5)],
-                          coerce=int)
-    submit = SubmitField(u'Submit')
+    implementation = RadioField(u'Implementation',
+                                choices=[(i, i) for i in range(0, 5)],
+                                coerce=int)
+    sharing = RadioField(u'Sharing',
+                         choices=[(i, i) for i in range(0, 5)],
+                         coerce=int)
+    creativity = RadioField(u'Creativity',
+                            choices=[(i, i) for i in range(0, 5)],
+                            coerce=int)
+    effectiveness = RadioField(u'Effectiveness',
+                               choices=[(i, i) for i in range(0, 5)],
+                               coerce=int)
+    inclusion = RadioField(u'Inclusion',
+                           choices=[(i, i) for i in range(0, 5)],
+                           coerce=int)
+    respect = RadioField(u'Respect',
+                         choices=[(i, i) for i in range(0, 5)],
+                         coerce=int)
 
     # TODO this code is duplicated in each judging form class, combine?
     def validate(self):
@@ -66,22 +65,21 @@ class PresentationForm(Form):
 
 class TeamworkForm(Form):
     team_id = SelectField(u'Team', coerce=int)
-    effectiveness = SelectField(u'Effectiveness',
-                                choices=[(i, i) for i in range(0, 5)],
-                                coerce=int)
-    efficiency = SelectField(u'Efficiency',
-                             choices=[(i, i) for i in range(0, 5)],
-                             coerce=int)
-    kids_do_the_work = SelectField(u'Kids do the work',
-                                   choices=[(i, i) for i in range(0, 5)],
-                                   coerce=int)
-    inclusion = SelectField(u'Inclusion',
+    effectiveness = RadioField(u'Effectiveness',
+                               choices=[(i, i) for i in range(0, 5)],
+                               coerce=int)
+    efficiency = RadioField(u'Efficiency',
                             choices=[(i, i) for i in range(0, 5)],
                             coerce=int)
-    respect = SelectField(u'Respect',
-                          choices=[(i, i) for i in range(0, 5)],
-                          coerce=int)
-    submit = SubmitField(u'Submit')
+    kids_do_the_work = RadioField(u'Kids do the work',
+                                  choices=[(i, i) for i in range(0, 5)],
+                                  coerce=int)
+    inclusion = RadioField(u'Inclusion',
+                           choices=[(i, i) for i in range(0, 5)],
+                           coerce=int)
+    respect = RadioField(u'Respect',
+                         choices=[(i, i) for i in range(0, 5)],
+                         coerce=int)
 
     def validate(self):
         # Base validation
@@ -105,34 +103,33 @@ class TeamworkForm(Form):
 
 class TechnicalForm(Form):
     team_id = SelectField(u'Team', coerce=int)
-    mechanical_durability = SelectField(u'Mechanical durability',
+    mechanical_durability = RadioField(u'Mechanical durability',
+                                       choices=[(i, i) for i in range(0, 5)],
+                                       coerce=int)
+    mechanical_efficiency = RadioField(u'Mechanical efficiency',
+                                       choices=[(i, i) for i in range(0, 5)],
+                                       coerce=int)
+    mechanization = RadioField(u'Mechanization',
+                               choices=[(i, i) for i in range(0, 5)],
+                               coerce=int)
+    programming_quality = RadioField(u'Programming quality',
+                                     choices=[(i, i) for i in range(0, 5)],
+                                     coerce=int)
+    programming_efficiency = RadioField(u'Programming efficiency',
                                         choices=[(i, i) for i in range(0, 5)],
                                         coerce=int)
-    mechanical_efficiency = SelectField(u'Mechanical efficiency',
-                                        choices=[(i, i) for i in range(0, 5)],
-                                        coerce=int)
-    mechanization = SelectField(u'Mechanization',
+    autonomous_navigation = RadioField(u'Autonomous navigation',
+                                       choices=[(i, i) for i in range(0, 5)],
+                                       coerce=int)
+    design_process = RadioField(u'Design process',
                                 choices=[(i, i) for i in range(0, 5)],
                                 coerce=int)
-    programming_quality = SelectField(u'Programming quality',
-                                      choices=[(i, i) for i in range(0, 5)],
-                                      coerce=int)
-    programming_efficiency = SelectField(u'Programming efficiency',
-                                         choices=[(i, i) for i in range(0, 5)],
-                                         coerce=int)
-    autonomous_navigation = SelectField(u'Autonomous navigation',
-                                        choices=[(i, i) for i in range(0, 5)],
-                                        coerce=int)
-    design_process = SelectField(u'Design process',
-                                 choices=[(i, i) for i in range(0, 5)],
-                                 coerce=int)
-    mission_strategy = SelectField(u'Mission strategy',
-                                   choices=[(i, i) for i in range(0, 5)],
-                                   coerce=int)
-    innovation = SelectField(u'Innovation',
-                             choices=[(i, i) for i in range(0, 5)],
-                             coerce=int)
-    submit = SubmitField(u'Submit')
+    mission_strategy = RadioField(u'Mission strategy',
+                                  choices=[(i, i) for i in range(0, 5)],
+                                  coerce=int)
+    innovation = RadioField(u'Innovation',
+                            choices=[(i, i) for i in range(0, 5)],
+                            coerce=int)
 
     def validate(self):
         # Base validation
@@ -156,10 +153,9 @@ class TechnicalForm(Form):
 
 class TeamSpiritForm(Form):
     team_id = SelectField(u'Team', coerce=int)
-    inspiration = SelectField(u'Inspiration',
-                              choices=[(i, i) for i in range(0, 5)],
-                              coerce=int)
-    submit = SubmitField(u'Submit')
+    inspiration = RadioField(u'Inspiration',
+                             choices=[(i, i) for i in range(0, 5)],
+                             coerce=int)
 
     def validate(self):
         # Base validation

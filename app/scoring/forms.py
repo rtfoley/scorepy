f044d0759a46d07ba6bfa3bde7c237a66b291571
Ryan Foley <rtfoley@gmail.com>
2015-11-12 12:48:32 -0500

    Update views and form with 2015 fields

--- app/scoring/forms.py
+++ app/scoring/forms.py
@@ -1,5 +1,5 @@
 from flask.ext.wtf import Form
-from wtforms import SelectField, RadioField
+from wtforms import SelectField, RadioField, IntegerField
 from models import RobotScore
 
 
@@ -8,19 +8,114 @@ class ScoreForm(Form):
     round_number = SelectField(u'Round',
                                choices=[(1, '1'), (2, '2'), (3, '3')],
                                coerce=int)
-    tree_branch_is_closer = RadioField(u'Is tree branch closer to mat than \
-                                       power lines',
+    # M04 yellow/ blue bars
+    bars_in_west_transfer = IntegerField('Yellow/ Blue Bars in matching green bins completely on/in West Transfer',
+                                         default=0)
+    bars_never_in_west_transfer = IntegerField(
+        'Yellow/ Blue Bars in matching green bins NEVER completely on/in West Transfer ', default=0)
+
+    # M04 black bars
+    black_bars_in_original_position = IntegerField('Black Bars in original position / scoring Flower Box', default=12)
+    black_bars_in_green_or_landfill = IntegerField('Black bars in matching Green Bin or Landfill', default=0)
+    black_bars_elsewhere = IntegerField('Black bars elsewhere in play', default=0)
+
+    # M02 Methane
+    methane_in_truck_or_factory = RadioField(u'Methane in Truck and/or Factory',
+                                             choices=[(0, '0'),
+                                                      (1, '1'),
+                                                      (2, '2')],
+                                             coerce=int,
+                                             default=0)
+
+    # M03 Transport
+    truck_supports_yellow_bin = RadioField(u'Truck supports all of Yellow Bin\'s weight',
+                                           choices=[('False', 'No'), ('True', 'Yes')],
+                                           default='False')
+    yellow_bin_east_of_guide = RadioField(u'Yellow Bin completely East of Truck\'s guide',
+                                          choices=[('False', 'No'), ('True', 'Yes')],
+                                          default='False')
+
+    # M05 Careers
+    anyone_in_sorter_area = RadioField(u'1+ people completely in Sorter Area',
                                        choices=[('False', 'No'), ('True', 'Yes')],
                                        default='False')
-    tree_branch_is_intact = RadioField(u'Is tree branch model intact',
-                                       choices=[('False', 'No'), ('True', 'Yes')],
-                                       default='False')
-    cargo_plane_location = RadioField(u'Cargo plane location',
-                                      choices=[(0, 'None'),
-                                               (1, 'Yellow only'),
-                                               (2, 'Light blue')],
-                                      coerce=int,
-                                      default=0)
+
+    # M06 Scrap Cars
+    engine_installed = RadioField(u'Engine/ Windshield installed in unfolded car',
+                                  choices=[('False', 'No'), ('True', 'Yes')],
+                                  default='False')
+    car_folded_in_east_transfer = RadioField(u'Car completely folded in East Transfer',
+                                             choices=[('False', 'No'), ('True', 'Yes')],
+                                             default='False')
+    car_never_in_safety = RadioField(u'Car never even partly in Safety',
+                                     choices=[('False', 'No'), ('True', 'Yes')],
+                                     default='False')
+
+    # M08 Composting
+    compost_ejected_not_in_safety = RadioField(u'Compost ejected, NOT completely in Safety',
+                                               choices=[('False', 'No'), ('True', 'Yes')],
+                                               default='False')
+    compost_ejected_in_safety = RadioField(u'Compost ejected, AND completely in Safety',
+                                           choices=[('False', 'No'), ('True', 'Yes')],
+                                           default='False')
+
+    # M07 Cleanup
+    plastic_bags_in_safety = RadioField(u'Plastic Bags completely in Safety',
+                                        choices=[(0, '0'),
+                                                 (1, '1'),
+                                                 (2, '2')],
+                                        coerce=int,
+                                        default=0)
+    animals_in_circles_without_bags = RadioField(u'Animals completely in any Circle without Bags',
+                                                 choices=[(0, '0'),
+                                                          (1, '1'),
+                                                          (2, '2'),
+                                                          (3, '3')],
+                                                 coerce=int,
+                                                 default=0)
+    chicken_in_small_landfill_circle = RadioField(u'Chicken completely in Small Landfill Circle',
+                                                  choices=[('False', 'No'), ('True', 'Yes')],
+                                                  default='False')
+
+    # M10 Demolition
+    all_beams_not_in_setup_position = RadioField(u'All Beams no longer in setup position',
+                                                 choices=[('False', 'No'), ('True', 'Yes')],
+                                                 default='False')
+
+    # M01 Recycled Material
+    green_bins_in_opp_safety = RadioField(u'Your Green Bins w/ Matching Yellow/ Blue bars in Opposing Safety',
+                                          choices=[(0, '0'),
+                                                   (1, '1'),
+                                                   (2, '2')],
+                                          coerce=int,
+                                          default=0)
+    opp_green_bins_in_safety = RadioField(u'Opposing Green Bins w/ Matching Yellow/ Blue bars in Your Safety',
+                                          choices=[(0, '0'),
+                                                   (1, '1'),
+                                                   (2, '2')],
+                                          coerce=int,
+                                          default=0)
+
+    # M09 Salvage
+    valuables_in_safety = RadioField(u'Valuables completely in Safety',
+                                     choices=[('False', 'No'), ('True', 'Yes')],
+                                     default='False')
+
+    # M11 Purchasing Decisions
+    planes_in_safety = RadioField(u'Toy Planes completely in Safety',
+                                  choices=[(0, '0'),
+                                           (1, '1'),
+                                           (2, '2')],
+                                  coerce=int,
+                                  default=0)
+
+    # M12 Repurposing
+    compost_in_toy_package = RadioField(u'Compost perfectly nested in empty Toy Package',
+                                        choices=[('False', 'No'), ('True', 'Yes')],
+                                        default='False')
+    package_in_original_condition = RadioField(u'Package in original condition',
+                                               choices=[('False', 'No'), ('True', 'Yes')],
+                                               default='False')
 
     def __init__(self, *args, **kwargs):
         Form.__init__(self, *args, **kwargs)
--- app/scoring/models.py
+++ app/scoring/models.py
@@ -9,11 +9,6 @@ class RobotScore(db.Model):
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
     round_number = db.Column(db.Integer)
 
-    # 2014 prototype data, remove once all 2015 data in place
-    tree_branch_is_closer = db.Column(db.Boolean)
-    tree_branch_is_intact = db.Column(db.Boolean)
-    cargo_plane_location = db.Column(db.Integer)
-
     # M04 yellow/ blue bars
     bars_in_west_transfer = db.Column(db.Integer)
     bars_never_in_west_transfer = db.Column(db.Integer)
@@ -67,20 +62,110 @@ class RobotScore(db.Model):
     def __init__(self,
                  team=0,
                  round_number=1,
-                 tree_branch_is_closer=False,
-                 tree_branch_is_intact=False,
-                 cargo_plane_location=0):
+                 # M04 yellow/ blue bars
+                 bars_in_west_transfer=0,
+                 bars_never_in_west_transfer=0,
+
+                 # M04 black bars
+                 black_bars_in_original_position=12,
+                 black_bars_in_green_or_landfill=0,
+                 black_bars_elsewhere=0,
+
+                 # M02 Methane
+                 methane_in_truck_or_factory=0,
+
+                 # M03 Transport
+                 truck_supports_yellow_bin=False,
+                 yellow_bin_east_of_guide=False,
+
+                 # M05 Careers
+                 anyone_in_sorter_area=False,
+
+                 # M06 Scrap Cars
+                 engine_installed=False,
+                 car_folded_in_east_transfer=False,
+                 car_never_in_safety=False,
+
+                 # M08 Composting
+                 compost_ejected_not_in_safety=False,
+                 compost_ejected_in_safety=False,
+
+                 # M07 Cleanup
+                 plastic_bags_in_safety=0,
+                 animals_in_circles_without_bags=0,
+                 chicken_in_small_landfill_circle=False,
+
+                 # M10 Demolition
+                 all_beams_not_in_setup_position=False,
+
+                 # M01 Recycled Material
+                 green_bins_in_opp_safety=0,
+                 opp_green_bins_in_safety=0,
+
+                 # M09 Salvage
+                 valuables_in_safety=False,
+
+                 # M11 Purchasing Decisions
+                 planes_in_safety=0,
+
+                 # M12 Repurposing
+                 compost_in_toy_package=False,
+                 package_in_original_condition=False):
         self.team_id = team
         self.round_number = round_number
-        self.tree_branch_is_closer = tree_branch_is_closer
-        self.tree_branch_is_intact = tree_branch_is_intact
-        self.cargo_plane_location = cargo_plane_location
+
+        # M04 yellow/ blue bars
+        self.bars_in_west_transfer = bars_in_west_transfer
+        self.bars_never_in_west_transfer = bars_never_in_west_transfer
+
+        # M04 black bars
+        self.black_bars_in_original_position = black_bars_in_original_position
+        self.black_bars_in_green_or_landfill = black_bars_in_green_or_landfill
+        self.black_bars_elsewhere = black_bars_elsewhere
+
+        # M02 Methane
+        self.methane_in_truck_or_factory = methane_in_truck_or_factory
+
+        # M03 Transport
+        self.truck_supports_yellow_bin = truck_supports_yellow_bin
+        self.yellow_bin_east_of_guide = yellow_bin_east_of_guide
+
+        # M05 Careers
+        self.anyone_in_sorter_area = anyone_in_sorter_area
+
+        # M06 Scrap Cars
+        self.engine_installed = engine_installed
+        self.car_folded_in_east_transfer = car_folded_in_east_transfer
+        self.car_never_in_safety = car_never_in_safety
+
+        # M08 Composting
+        self.compost_ejected_not_in_safety = compost_ejected_not_in_safety
+        self.compost_ejected_in_safety = compost_ejected_in_safety
+
+        # M07 Cleanup
+        self.plastic_bags_in_safety = plastic_bags_in_safety
+        self.animals_in_circles_without_bags = animals_in_circles_without_bags
+        self.chicken_in_small_landfill_circle = chicken_in_small_landfill_circle
+
+        # M10 Demolition
+        self.all_beams_not_in_setup_position = all_beams_not_in_setup_position
+
+        # M01 Recycled Material
+        self.green_bins_in_opp_safety = green_bins_in_opp_safety
+        self.opp_green_bins_in_safety = opp_green_bins_in_safety
+
+        # M09 Salvage
+        self.valuables_in_safety = valuables_in_safety
+
+        # M11 Purchasing Decisions
+        self.planes_in_safety = planes_in_safety
+
+        # M12 Repurposing
+        self.compost_in_toy_package = compost_in_toy_package
+        self.package_in_original_condition = package_in_original_condition
 
     def getScore(self):
         score = 0
-        if self.tree_branch_is_closer and self.tree_branch_is_intact:
-            score += 30
-        score += self.get_plane_score(self.cargo_plane_location)
         return score
 
     def get_plane_score(self, argument):
@@ -88,5 +173,5 @@ class RobotScore(db.Model):
             0: 0,
             1: 20,
             2: 30,
-            }
+        }
         return switcher.get(argument, 0)

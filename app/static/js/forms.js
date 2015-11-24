6f539757bd535bec256cb887a217389d60199d44
Ryan Foley <rtfoley@gmail.com>
2015-11-23 23:14:52 -0500

    Change team and round select fields to use selectize library

--- app/static/js/forms.js
+++ app/static/js/forms.js
@@ -1,6 +1,20 @@
 // Use new select2 control for team or round
 $( document ).ready(function() {
     // TODO setup fancy select fields
+    $("#team_id").prepend("<option value='' selected='selected'></option>");
+    $("#team_id").selectize({
+        placeholder: "Select a team",
+        openOnFocus: false,
+        selectOnTab: true
+    });
+
+
+    $("#round_number").prepend("<option value='' selected='selected'></option>");
+    $("#round_number").selectize({
+        placeholder: "Select a round",
+        openOnFocus: false,
+        selectOnTab: true
+    });
 
     // Focus on first element in the form
     $(":input:visible:first").focus();

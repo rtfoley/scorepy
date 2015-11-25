cdf6aa895ffdc82d3ad2f47224d46b3dedf78989
Ryan Foley <rtfoley@gmail.com>
2015-11-25 12:57:54 -0500

    Update awards form to only prepend a placeholder if the form is not an 'edit'

--- app/static/js/forms.js
+++ app/static/js/forms.js
@@ -1,21 +1,18 @@
 // Use new select2 control for team or round
 $( document ).ready(function() {
-    // TODO setup fancy select fields
-    $("#team_id").prepend("<option value='' selected='selected'></option>");
+    // Don't set a placeholder on the select fields if this is an edit form
+    if($('.form').attr("action") != "edit") {
+        $("#team_id").prepend("<option value='' selected='selected'></option>");
+        $("#round_number").prepend("<option value='' selected='selected'></option>");
+    }
+
     $("#team_id").selectize({
         placeholder: "Select a team",
-        openOnFocus: false,
         selectOnTab: true
     });
 
-
-    $("#round_number").prepend("<option value='' selected='selected'></option>");
     $("#round_number").selectize({
         placeholder: "Select a round",
-        openOnFocus: false,
         selectOnTab: true
     });
-
-    // Focus on first element in the form
-    $(":input:visible:first").focus();
 });

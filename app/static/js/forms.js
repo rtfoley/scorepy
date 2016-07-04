e08fe1d1e5999a2de9cba96efea4de084a0a8658
rtfoley <rtfoley@gmail.com>
2016-07-04 18:39:36 -0400

    Make forms fit without scrolling, fixes #27 (#37)
    * Convert score form to select fields
    
    * Convert judging forms over to select fields for all inputs
    
    * Focus first input on form load, and remove selective from forms
    
    * Remove y-scroll and put selectors at top
    
    * Move inputs to pull-right
    
    * Remove the panels from score form
    
    * Make form labels not bold
    
    * Remove duplicate label tags
    
    * Shrink inputs
    
    * Group missions into two columns using small form groups
    
    * Remove section headers from forms
    
    * Fix team/ round input formatting at top of score form
    
    * Add mission numbers to score form fields
    
    * Fix selector formatting on judging forms.

--- app/static/js/forms.js
+++ app/static/js/forms.js
@@ -1,12 +1,4 @@
 // Use new select2 control for team or round
 $( document ).ready(function() {
-    $("#team_id").selectize({
-        placeholder: "Select a team",
-        selectOnTab: true
-    });
-
-    $("#round_number").selectize({
-        placeholder: "Select a round",
-        selectOnTab: true
-    });
+    $('form:first *:input[type!=hidden]:first').focus();
 });

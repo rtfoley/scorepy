49a38908552e02e816c38ba1b63de1655a454c40
Ryan Foley <rtfoley@gmail.com>
2016-07-06 00:19:17 -0400

    Don't track sqlalchemy modifications

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ _cwd = dirname(abspath(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + join(_cwd, 'scorepy.db')
 #SQLALCHEMY_ECHO = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'

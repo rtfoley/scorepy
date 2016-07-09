163369159a72cde74a1c33ad4dbdc8a061947c12
Ryan Foley <rtfoley@gmail.com>
2016-07-09 01:59:02 -0400

    Put run back to production server

--- run.py
+++ run.py
@@ -3,12 +3,11 @@ from app import app
 import jinja2.ext
 
 # from http://flask.pocoo.org/snippets/24/
-#d = wsgiserver.WSGIPathInfoDispatcher({'/': app})
-#server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 8080), d)
+d = wsgiserver.WSGIPathInfoDispatcher({'/': app})
+server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 8080), d)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True)
-#   try:
-#      server.start()
-#   except KeyboardInterrupt:
-#      server.stop()
+   try:
+      server.start()
+   except KeyboardInterrupt:
+      server.stop()

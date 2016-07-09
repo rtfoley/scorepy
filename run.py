1665bc87cf416a6c891dd5d419f8b6b5dbbba620
Ryan Foley <rtfoley@gmail.com>
2016-07-09 01:55:56 -0400

    fix page title sizes

--- run.py
+++ run.py
@@ -3,11 +3,12 @@ from app import app
 import jinja2.ext
 
 # from http://flask.pocoo.org/snippets/24/
-d = wsgiserver.WSGIPathInfoDispatcher({'/': app})
-server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 8080), d)
+#d = wsgiserver.WSGIPathInfoDispatcher({'/': app})
+#server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 8080), d)
 
 if __name__ == '__main__':
-   try:
-      server.start()
-   except KeyboardInterrupt:
-      server.stop()
+    app.run('0.0.0.0', debug=True)
+#   try:
+#      server.start()
+#   except KeyboardInterrupt:
+#      server.stop()

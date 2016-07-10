f3e4f0c44e40d514b3929a6dce973c7b25feee44
Ryan Foley <rtfoley@gmail.com>
2016-07-10 17:12:04 -0400

    Fade-out messages instead of sliding up

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   window.setTimeout(function() {
-      $(".alert-success").fadeTo(1000, 0).slideUp(1000, function(){
+      $(".alert-success").fadeTo(1000, 0).fadeOut(1000, function(){
           $(this).remove();
       });
   }, 3000);

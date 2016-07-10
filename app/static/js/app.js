78f383bbf13cd5183254260c7a759068d3045ad7
Ryan Foley <rtfoley@gmail.com>
2016-07-10 16:59:56 -0400

    Auto-dismiss success messages

---
+++ app/static/js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  window.setTimeout(function() {
+      $(".alert-success").fadeTo(1000, 0).slideUp(1000, function(){
+          $(this).remove();
+      });
+  }, 3000);
+});

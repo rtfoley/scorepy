423328292ea735c01394537a7439b2316fc1eb4b
Ryan Foley <rtfoley@gmail.com>
2016-07-10 17:17:28 -0400

    Fade alerts in/out faster

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   window.setTimeout(function() {
-      $(".alert-success").fadeTo(1000, 0).fadeOut(1000, function(){
+      $(".alert-success").fadeTo(0, 0).fadeOut(500, function(){
           $(this).remove();
       });
-  }, 3000);
+  }, 2000);
 });

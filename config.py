a166eb93e273a7220e37d61f45362289b28dd0eb
Ryan Foley <rtfoley@gmail.com>
2016-07-18 21:20:01 -0400

    Add basic logging to file

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from os.path import abspath, dirname, join
+import logging
 
 _cwd = dirname(abspath(__file__))
 
@@ -8,6 +9,10 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + join(_cwd, 'scorepy.db')
 #SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOGGING_LOCATION = 'scorepy.log'
+LOGGING_LEVEL = logging.INFO
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'

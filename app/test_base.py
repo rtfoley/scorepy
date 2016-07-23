e556a6e5b74571b8286d7e59c34e55bc1df5bb8c
Ryan Foley <rtfoley@gmail.com>
2016-07-22 23:05:40 -0400

    Add login methods

--- app/test_base.py
+++ app/test_base.py
@@ -6,13 +6,22 @@ class BaseTestCase(TestCase):
     def create_app(self):
         app.config.from_object('config.TestingConfiguration')
         return app
-    
+
     def setUp(self):
         db.create_all()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+
+    def login(self, username, password):
+        return self.client.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.client.get('/logout', follow_redirects=True)
 
 if __name__ == '__main__':
     unittest.main()

d773ed62a2fafc7531d79bbf41be5b712e04a741
Ryan Foley <rtfoley@gmail.com>
2016-07-22 23:05:09 -0400

    Leverage flask-testing assert200

--- app/tests.py
+++ app/tests.py
@@ -3,4 +3,4 @@ from app.test_base import BaseTestCase
 class TestTopLevelFunctions(BaseTestCase):
     def test_index_response(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assert200(response)
